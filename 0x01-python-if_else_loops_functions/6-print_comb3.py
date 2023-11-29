3db261841ffe52df00ab31ed40ae69c89ff1b903
OrisonXmef <orisonmustapha@gmail.com>
2023-11-29 08:40:28 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for num1 in range(10):
-    for num2 in range(num1 + 1, 10):
-        print("{:d}{:d}".format(num1, num2), end=", " if (num1, num2) != (8, 9) else "\n")
+for n1 in range(10):
+    for n2 in range(n1 + 1, 10):
+    print("{:d}{:d}".format(n1, n2), end=", " if (n1, n2) != (8, 9) else "\n")
