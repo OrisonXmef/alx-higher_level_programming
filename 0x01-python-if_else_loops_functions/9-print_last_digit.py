40c00c6674fe067f4881ea38041c8ccc4528bbd9
OrisonXmef <orisonmustapha@gmail.com>
2023-11-29 07:04:17 -0800

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    print(last_digit, end='')
+    return last_digit
+
+def main():
+    print_last_digit(98)
+    print_last_digit(0)
+    r = print_last_digit(-1024)
+    print(r)
+
+if __name__ == "__main__":
+    main()
+
