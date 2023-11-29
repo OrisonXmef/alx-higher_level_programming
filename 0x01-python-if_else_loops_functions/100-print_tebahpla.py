4dc2964ffb674f92377aaf54b63f911643626df9
OrisonXmef <orisonmustapha@gmail.com>
2023-11-29 07:57:22 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(90, 64, -1):
-    print("{:c}".format(i), end="")
+for i in range(90, 64, -2):
     print("{:c}".format(i + 32), end="")
+    print("{:c}".format(i), end="")
 
 print()
-
