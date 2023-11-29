3840da84e17d5ebbfad9517c7dfaa794f0598ea7
OrisonXmef <orisonmustapha@gmail.com>
2023-11-29 05:47:06 -0800

    3-print_alphabt.py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for c in range(ord('a'), ord('z') + 1):
+    if c != ord('q') and c != ord('e'):
+        print("{:c}".format(c), end='')
+
+print()  # Print a newline at the end
