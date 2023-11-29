b07df6f53dab97aad8460e6e72923283e10b04fe
OrisonXmef <orisonmustapha@gmail.com>
2023-11-29 06:39:13 -0800

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,18 @@
+def uppercase(s):
+    for char in s:
+        # Check if the character is a lowercase letter
+        if 'a' <= char <= 'z':
+            # Convert the lowercase letter to uppercase using ASCII values
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+            print(uppercase_char, end='')
+        else:
+            # Print the character as is
+            print(char, end='')
+
+    # Print a new line after processing the string
+    print()
+
+# Example usage:
+text = "Hello, World!"
+uppercase(text)
+
