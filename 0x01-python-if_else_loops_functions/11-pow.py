e600300435133ca84fc3c2a6729b2cddcd8a6058
OrisonXmef <orisonmustapha@gmail.com>
2023-11-29 07:38:20 -0800

    11-pow.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    return a ** b
+
+if __name__ == "__main__":
+    pow = __import__('11-pow').pow
+
+    print(pow(2, 3))
+    print(pow(98, 2))
+    print(pow(5, 0))
