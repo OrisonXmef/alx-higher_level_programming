708fd7103cda6000173424543ccd5a52afdacbf3
OrisonXmef <orisonmustapha@gmail.com>
2023-12-05 14:13:51 -0800

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
+
+# Example usage:
+my_list = [1, 2, 3, 4, 5]
+index = 2
+result = element_at(my_list, index)
+print("Element at index {}: {}".format(index, result))
