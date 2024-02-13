6e75ceb57d624bab0d1fbbf00d2cf201aca3d5b2
OrisonXmef <orisonmustapha@gmail.com>
2024-02-13 09:10:16 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,7 +1,5 @@
--- Creates and fills a table second_table with attributes id, name and score in my MySQL server with multiple rows.
-CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+-- Displays the number of records with id = 89 in the table first_table in my MySQL server.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
 
